b67a48da46ca328667c30b32dca5109a8bedf8a0
archanjo <alvaroarchanjojunior@gmail.com>
2024-01-17 21:54:38 -0300

    commit 5

--- python_script/connector.py
+++ python_script/connector.py
@@ -8,5 +8,37 @@ conn = mysql.connector.connect(
     port='3307'
 )
 
-if conn.is_connected():
-    print('CONECTAMOS!!!') 
+# if conn.is_connected():
+#     print('CONECTAMOS!!!') 
+
+
+print("Este é o melhor jogo de Pedra, Papel e Tesoura!")
+
+print("Escolha sua jogada: PEDRA, PAPEL ou TESOURA")
+
+jogador = input()
+
+print("Asua jogada foi:", jogador)
+
+computador = "PEDRA"
+
+if computador == "PEDRA":
+    if jogador == "PEDRA":
+        resultado = "EMPATE"
+    else:
+        if jogador == "PAPEL":
+            resultado = "VITORIA"
+        else:
+            resultado = "DERROTA"
+
+print(resultado)
+
+cursor = conn.cursor()
+query = """
+    INSERT INTO contagem_jogos (nome_player, jogada_player, jogada_cpu, resultado)
+VALUES ('jogador', 'PEDRA', 'PEDRA', 'E')
+"""
+
+cursor.execute(query)
+conn.commit()
+cursor.close()
