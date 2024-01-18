b2acab4f1a5f983a0eb0827b48525343790266b7
archanjo <alvaroarchanjojunior@gmail.com>
2024-01-17 22:27:43 -0300

    melhoria jogo

--- python_script/connector.py
+++ python_script/connector.py
@@ -14,31 +14,41 @@ conn = mysql.connector.connect(
 
 print("Este é o melhor jogo de Pedra, Papel e Tesoura!")
 
-print("Escolha sua jogada: PEDRA, PAPEL ou TESOURA")
+print("Por favor insira seu nome:")
+nome_jogador = input()
 
-jogador = input()
+# import random
+# computador = random 
 
-print("Asua jogada foi:", jogador)
+while True
+    print("Escolha sua jogada: PEDRA, PAPEL ou TESOURA")
 
-computador = "PEDRA"
+    jogador = input()
 
-if computador == "PEDRA":
-    if jogador == "PEDRA":
-        resultado = "EMPATE"
-    else:
-        if jogador == "PAPEL":
-            resultado = "VITORIA"
+    print("Asua jogada foi:", jogador)
+
+    computador = "PEDRA"
+
+    if computador == "PEDRA":
+        if jogador == "PEDRA":
+            resultado = "EMPATE"
         else:
-            resultado = "DERROTA"
+            if jogador == "PAPEL":
+                resultado = "VITORIA"
+            else:
+                resultado = "DERROTA"
 
-print(resultado)
+    print(resultado)
 
-cursor = conn.cursor()
-query = """
-    INSERT INTO contagem_jogos (nome_player, jogada_player, jogada_cpu, resultado)
-VALUES ('jogador', 'PEDRA', 'PEDRA', 'E')
-"""
+    cursor = conn.cursor()
+    query = f"""
+        INSERT INTO contagem_jogos (nome_player, jogada_player, jogada_cpu, resultado)
+    VALUES ('{nome_jogadorr}', '{jogador}', '{computador}', '{resultado[0]}')
+    """
 
-cursor.execute(query)
-conn.commit()
-cursor.close()
+    cursor.execute(query)
+    conn.commit()
+    cursor.close()
+    print("Deseja continuar jogando (Y/N)?")
+    continuar = input()
+    if continuar =="N":
